6eb0a6b140efa4d8a4933c719c9ecf781dcf0437
Юшков ИВБО-01-21 <yasenevakursa@gmail.com>
2022-10-26 10:37:38 +0300

    №1.3

---
+++ src/ru/mirea/lab1/Mirea.java
@@ -0,0 +1,14 @@
+package ru.mirea.lab1;
+import java.util.Scanner;
+public class Mirea {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        float x = 0;
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 10; i++){
+            numbers[i] = sc.nextInt();
+            x += numbers[i];
+        }
+        System.out.println(x / 10);
+    }
+}
